4e2830cc8c30cde18b57c7b8f53c26f7941eddc1
iSIayer <92229337+iSIayer@users.noreply.github.com>
2023-02-05 19:27:05 +0000

    Deploying to gh-pages from @ iSIayer/calculator-of-price@3f508fa45be30c20f97fe1a64e35b599787d9325 🚀

---
+++ index.1c9b7f74.js
@@ -0,0 +1,2 @@
+!function(){console.log(Object.keys({scaleway:{price:{storage:{multi:.06,single:.03},transfer:.02,free:75},slug:"scaleway.com"},bunny:{price:{storage:{HDD:.01,SSD:.02},transfer:.01,max:10},slug:"bunny.net"},vultr:{price:{storage:.01,transfer:.01,min:5},slug:"vultr.com"},backblaze:{price:{storage:.005,transfer:.01,min:7},slug:"backblaze.com"}}));document.querySelector("#storage-range"),document.querySelector("#transfer-range")}();
+//# sourceMappingURL=index.1c9b7f74.js.map
